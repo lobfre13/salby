0afb32cbc242b1c3218bf1d81c518919788c7214
Fredrik Løberg <lobfre13@nith.no>
2015-02-25 12:51:17 +0100

    Elevforside oppdatert

--- app/views/main.php
+++ app/views/main.php
@@ -9,8 +9,19 @@
         <?php foreach($subjects as $subject) { ?>
             <div id="<?php echo $subject['subjectname'];?>" class="subjectContent">
                 <?php foreach($lobjects as $lobject) {
-                if($lobject['subjectid'] === $subject['subjectid']) echo '<a href="/main/category/'.$lobject['categoryid'].'"><div class="category" style="background-image: url('.$lobject['imgurl'].');"><h4>'.$lobject['category'].'</h4></div></a>'; } ?>
+                    if($lobject['subjectid'] === $subject['subjectid']) echo '<a href="#'.$lobject['category'].'"><div class="category" style="background-image: url('.$lobject['imgurl'].');"><h4 class="categoryname">'.$lobject['category'].'</h4></div></a>';
+                } ?>
+            </div>
+
+        <?php } ?>
+
+        <?php foreach($lobjects as $lobject){ ?>
+            <div id="<?php echo $lobject['category']; ?>" class="subjectContent">
+                <?php foreach($lobjects as $llobject){ ?>
+                    <?php if($llobject['categoryid'] == $lobject['categoryid']) echo '<div class="category" style="background-image: url('.$lobject['imgurl'].');">'.$llobject['title'].'</div>';?>
+                <?php } ?>
             </div>
         <?php } ?>
+
 
     </div>
