d3d5687160701d79377d9906674343eae61ff2a9
Fredrik Løberg <lobfre13@nith.no>
2015-02-25 13:23:44 +0100

    Elevforside oppdatert

--- app/views/main.php
+++ app/views/main.php
@@ -9,7 +9,7 @@
         <?php foreach($subjects as $subject) { ?>
             <div id="<?php echo $subject['subjectname'];?>" class="subjectContent">
                 <?php foreach($lobjects as $lobject) {
-                    if($lobject['subjectid'] === $subject['subjectid']) echo '<a href="#'.$lobject['category'].'"><div class="category" style="background-image: url('.$lobject['imgurl'].');"><h4 class="categoryname">'.$lobject['category'].'</h4></div></a>';
+                    if($lobject['subjectid'] == $subject['subjectid']) echo '<a href="#'.$lobject['category'].'"><div class="category" style="background-image: url('.$lobject['imgurl'].');"><h4 class="categoryname">'.$lobject['category'].'</h4></div></a>';
                 } ?>
             </div>
 
