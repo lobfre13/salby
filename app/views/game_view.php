a4b5c1de55095bc8d3ffb7993fac03fbdf276f7a
Fredrik Løberg <lobfre13@nith.no>
2015-02-26 10:19:05 +0100

    Lobjects implementet

--- app/views/game_view.php
+++ app/views/game_view.php
@@ -6,5 +6,14 @@
         </form>
 
         <h1>Her er læringsobjektet.</h1>
+
+        <iframe seamless src="<?php echo $lobject['link']; ?>" frameborder="none" scrolling="no" onload="resizeIframe(this)"></iframe>
     </div>
 </div>
+
+<script language="javascript" type="text/javascript">
+    function resizeIframe(obj) {
+        obj.style.height = obj.contentWindow.document.body.scrollHeight + 'px';
+        obj.style.width = obj.contentWindow.document.body.scrollWidth + 'px';
+    }
+</script>
