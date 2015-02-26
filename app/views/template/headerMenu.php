b5a8ec107e9e30bda4f0445291abd5cc2b099661
Fredrik <lobfre13@nith.no>
2015-02-26 19:31:12 +0100

    nye logoer

--- app/views/template/headerMenu.php
+++ app/views/template/headerMenu.php
@@ -4,11 +4,11 @@
 			<div></div><div></div>
 			<div id="dropDownMainMenu">
 				<ul>
-					<li>Barnehage<img src="/public/img/lock2.png"></li>
+					<li>Barnehage<img src="/public/img/lock.png"></li>
 					<li>Skolestart</li>
-					<li>1-2. Klasse<img src="/public/img/lock2.png"></li>
-					<li>3-4. Klasse<img src="/public/img/lock2.png"></li>
-					<li>5-7. Klasse<img src="/public/img/lock2.png"></li>
+					<li>1-2. Klasse<img src="/public/img/lock.png"></li>
+					<li>3-4. Klasse<img src="/public/img/lock.png"></li>
+					<li>5-7. Klasse<img src="/public/img/lock.png"></li>
 				</ul>
 			</div>
 		</div>
@@ -17,7 +17,7 @@
 
 		<div id="user">
 			<a href="/mypage"><img src="/public/img/profile.png"/> <span class="topMenuUser"><?php echo $_SESSION['user']->getUsername();?></span></a>
-            <a href="/logout"><img src="/public/img/logout2.png"> <span class="topMenuUser"> Logg ut</span></a>
+            <a href="/logout"><img src="/public/img/logout.png"> <span class="topMenuUser"> Logg ut</span></a>
         </div>
     </div>
 </div>
