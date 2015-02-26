1fa6c621a2017b4c61345b1bcf43af10ff23643b
Simen Fonnes <fonsim13@student.westerdals.no>
2015-02-26 16:00:55 +0100

    lagt til css for mypage

--- app/views/mypage_view.php
+++ app/views/mypage_view.php
@@ -1,24 +1,34 @@
 <div id="content" class="widthConstrained">
-    <p>
-        <?php
-        echo $username.' '.$schoolClass['classlevel'].$schoolClass['classname'];
-        ?>
-    </p>
+    <div id="usernameAndClass">
+        <p>
+            <?php
+                echo $username . ' ' . $schoolClass['classlevel'] . $schoolClass['classname'];
+            ?>
+        </p>
+    </div>
 
-    <ul>
-        <?php
-        foreach($homeworkList as $homeworkItem) {
-            echo '<li><a href="/game/id/'.$homeworkItem['learningobjectid'].'">'.$homeworkItem['title'].'</a></li>';
-        }
-        ?>
-    </ul>
+    <br>
 
-    <ul>
-        <?php
-        foreach($favouriteList as $favouriteItem) {
-            echo '<li><a href="/game/id/'.$favouriteItem['learningobjectid'].'">'.$favouriteItem['title'].'</a></li>';
-        }
-        ?>
-    </ul>
+    <div id="homeworkList">
+        <ul>
+            <?php
+            foreach ($homeworkList as $homeworkItem) {
+                echo '<li><a href="/game/id/' . $homeworkItem['learningobjectid'] . '">' . $homeworkItem['title'] . '</a></li>';
+            }
+            ?>
+        </ul>
+    </div>
+
+    <br>
+
+    <div id="favouriteList">
+        <ul>
+            <?php
+            foreach ($favouriteList as $favouriteItem) {
+                echo '<li><a href="/game/id/' . $favouriteItem['learningobjectid'] . '">' . $favouriteItem['title'] . '</a></li>';
+            }
+            ?>
+        </ul>
+    </div>
 </div>
 
