92204721d37df6ad3f1cd3145cf516c9c32e328b
Fredrik <lobfre13@nith.no>
2015-03-02 15:27:21 +0100

    Loginside responsiv

--- app/views/login.php
+++ app/views/login.php
@@ -1,23 +1,19 @@
-    <div id="loginBoxWrapper">
-        <div id="loginBoxBG"></div>
+
         <div id="loginBox">
             <a href="/"><img id="loginLogo"></a><br>
             <form method="post" id="login">
                 <p>Velkommen til Salaby.no!<br> For å få tilgang til innhold må du logge inn</p>
-                <?php
-                    if($failedLogin){
-                        echo "<p style=\"color:red;\">Feil Brukernavn eller passord!</p>";
-                    }
-                ?>
-                <label>Brukernavn:</label><br>
-                <input type="text" name="username" value="Brukernavn/Epost" onfocus="if (this.value == 'Brukernavn/Epost') { this.value = '';}"/><br>
-                <label>Passord:</label><br>
-                <input type="password" name="password" value="passord" onfocus="if (this.value == 'passord') { this.value = '';}"/><br>
+
+                <p style="color:red;"> <?php if($failedLogin)echo "Feil Brukernavn eller passord!"; ?></p>
+
+                <label>Brukernavn:<br>
+                <input type="text" name="username" value="Brukernavn/Epost" onfocus="if (this.value == 'Brukernavn/Epost') { this.value = '';}" onblur="if (this.value == '') { this.value = 'Brukernavn/Epost';}"/></label><br>
+                <label>Passord:<br>
+                <input type="password" name="password" value="passord" onfocus="if (this.value == 'passord') { this.value = '';}" onblur="if (this.value == '') { this.value = 'passord';}"/></label><br>
                 <input id="loginSubmit" type="submit" value="Logg Inn"/>
             </form>
-            <br><br>
+            <br>
             <a href="#">Glemt Passord?</a>   <a href="/register">Opprett ny bruker</a>
             <br><br>
             <a id="nonLogin" href="#">Fortsett uten innlogging --></a>
         </div>
-	</div>
