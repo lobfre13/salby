cbd0d59ca9ece32b1bf2c906e0d88675dbee5a4c
Simen Fonnes <fonsim13@student.westerdals.no>
2015-03-04 15:58:43 +0100

    Fikset at tekst forblir farge på menyen når valgt.

--- app/views/main.php
+++ app/views/main.php
@@ -13,10 +13,10 @@
             var subjects =  document.getElementsByClassName("subjectimg");
             for(var i = 0; i < subjects.length; i++){
                 subjects[i].style.opacity = opacityValue;
-                subjects[i].nextSibling.style.backgroundColor = "chocolate";
+                subjects[i].nextSibling.style.backgroundColor = "#88C34B";
             }
             obj.firstChild.style.opacity = "1";
-            obj.children[1].style.backgroundColor = "blue";
+            obj.children[1].style.backgroundColor = "#88C34B";
         }
     </script>
     <div id="content" class="widthConstrained">
