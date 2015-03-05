4958cf99a199b6d8caa1dc5e10caecff832bd0e9
Simen Fonnes <fonsim13@student.westerdals.no>
2015-03-05 10:33:54 +0100

    Lærersiden - view

---
+++ app/views/teacher/add_homework_view_1.php
@@ -0,0 +1,31 @@
+<div id="content" class="widthConstrained">
+
+    <div id = "homeworkListTop">Lekseliste</div>
+    <div id = "homeworkListTop">Velg elev</div>
+    <div id = "homeworkListTop">Godkjenn</div>
+
+    <p>Marker spillene fra listen som du vil legge til i elevens gjøremål</p>
+
+    <div id = "dropDownMenuClassAndView">
+        <select>
+            <option value="test1">test1</option>
+            <option value="test2">test2</option>
+            <option value="test3">test3</option>
+        </select>
+
+        <select>
+            <option value="test4">test4</option>
+            <option value="test5">test5</option>
+            <option value="test6">test6</option>
+        </select>
+    </div>
+
+    <div id = "homeworkListDiv">
+        Du har ingen spill i lekselisten
+    </div>
+
+    <div id = "continue">
+        <p>2 spill er markert, gå videre</p>
+        <img src="../../../public/img/hinduismespillet/Pil1.png"
+    </div>
+</div>
