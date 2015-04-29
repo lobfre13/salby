ad8cfa84247de1d4e3a97530897b84a201d2660c
Martin Moen Forsberg <formar13@nith.no>
2015-04-29 22:58:26 +0200

    fant et par filer jeg hadde glemt å adde før jeg commita. fixed

---
+++ app/views/admin/PartialViews/adminMenu.php
@@ -0,0 +1,15 @@
+    <section id="navMenuAdmin">
+        <nav id="primary_nav_wrap">
+            <ul>
+                <li><a href="/admin/administrateSchools">Brukeradministrasjon</a>
+
+                <li><a href="#">Sideadministrasjon</a>
+                    <ul class="sideAdminSub">
+                        <li><a href="/admin/administrateSubjects">Fag</a></li>
+                        <li><a href="/admin/administrateCategories">Kategorier</a></li>
+                        <li><a href="/admin/administrateLearningobjects">Læringsobjekter</a></li>
+                    </ul>
+                </li>
+            </ul>
+        </nav>
+    </section>
