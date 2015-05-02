5ada374fbf8ef02d1d77b2cb04163206107209b6
Simen Fonnes <fonsim13@student.westerdals.no>
2015-05-03 00:59:53 +0200

    vidreutviklling av teacher, pupilsettings

--- app/views/teacher/pupilSettingsTable.php
+++ app/views/teacher/pupilSettingsTable.php
@@ -1,7 +1,7 @@
 
     <tr>
-        <th>Etternavn</th>
         <th>Fornavn</th>
+        <th>Etternavn</th>
         <th>Brukernavn</th>
         <th>Passord</th>
         <th>E-post</th>
@@ -10,8 +10,8 @@
     </tr>
     <?php foreach ($this->pupils as $pupil) { ?>
     <tr>
-        <td><?php echo $pupil['lastname']; ?></td>
         <td><?php echo $pupil['firstname']; ?></td>
+        <td><?php echo $pupil['lastname']; ?></td>
         <td><?php echo $pupil['username']; ?></td>
         <td><?php echo $pupil['password']; ?></td>
         <td><?php echo $pupil['email']; ?></td>
--- app/views/teacher/pupilsettings.php
+++ app/views/teacher/pupilsettings.php
@@ -7,15 +7,17 @@
 <div id="content" class="widthConstrained">
 
     <h3>Dine elever</h3>
-    <h3>Velg klasse</h3>
-    <select id="selectSubjectDropDown" onchange="pupilsFromClass(this)">
+    <select id="selectSubjectDropDown" onchange="pupilsFromClass(this)" class="styled-select">
         <?php foreach($this->classes as $class) { ?>
-        <option value="<?php echo $class['id'];?>"><?php echo $class['classlevel'] . $class['classname']?></option>
+        <option value="<?php echo $class['id'];?>">Klasse <?php echo $class['classlevel'] . $class['classname']?></option>
         <?php } ?>
     </select>
+    <div class="tableBG">
     <table id = pupilSettingsDiv>
 
     </table>
+    </div>
+
 
     <script>$( document ).ready(function() {
             $("#selectSubjectDropDown").trigger('onchange');
