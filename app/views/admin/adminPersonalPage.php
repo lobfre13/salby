46eabf63881b84fb7959605779cc7b8b45397d0d
Simen Fonnes <fonsim13@student.westerdals.no>
2015-05-03 16:58:32 +0200

    admin - personlig side etter skisse. Done

--- app/views/admin/adminPersonalPage.php
+++ app/views/admin/adminPersonalPage.php
@@ -1,33 +1,34 @@
 <div class="widthConstrained" >
 
-    <h1><?php echo $this->admin['firstname'] . ' ' . $this->admin['lastname'] . ' (' . $this->admin['username'] . ')';?></h1>
+    <h1 id="adminPersonalPageHeadline"><?php echo $this->admin['firstname'] . ' ' . $this->admin['lastname'] . ' (' . $this->admin['username'] . ')';?></h1>
+
+    <div id="centerMyContent">
+
+        <div id="emailDiv" class="adminPersonalBox">
+            <h3>E-post</h3>
+            <form method="post" action="/admin/doChangeEmail">
+                <p id="currentEmail"><?php if (empty($this->admin['email'])) { echo 'Ingen e-postadresse satt enda.'; } else {echo $this->admin['email'];} ?></p>
+                <h3>Endre e-postadresse</h3>
+                <input name="email" type="email" placeholder="Skriv inn ny e-post..." class="smoothInputField" required>
+                <br>
+                <input type="submit" value="Endre" class="endreButton">
+            </form>
+        </div>
 
 
-    <div id="emailDiv">
-        <h3>Endre e-postadresse</h3>
-        <form method="post" action="/admin/doChangeEmail">
-            <p><?php if (empty($this->admin['email'])) { echo 'Ingen e-postadresse satt enda.'; } else {echo $this->admin['email'];} ?></p>
-            <input name="email" type="email" placeholder="Skriv inn ny e-post...">
-            <br>
-            <input type="submit" value="Endre">
-        </form>
+        <div id="passwordDiv" class="adminPersonalBox">
+            <h3>Endre passord</h3>
+            <form method="post" action="/admin/doChangePassword">
+                <input name="currentPassword" type="password" placeholder="Nåværende passord..." class="smoothInputField" required>
+                <br>
+                <input name="newPassword1" type="password" placeholder="Nytt passord..." class="smoothInputField" required>
+                <br>
+                <input name="newPassword2" type="password" placeholder="Gjenta passord..." class="smoothInputField" required>
+                <br>
+                <input type="submit" value="Endre" class="endreButton">
+            </form>
+        </div>
     </div>
-
-
-    <div id="passwordDiv">
-        <h3>Endre passord</h3>
-        <form method="post" action="/admin/doChangePassword">
-            <input name="currentPassword" type="password" placeholder="Nåværende passord...">
-            <br>
-            <input name="newPassword1" type="password" placeholder="Nytt passord...">
-            <br>
-            <input name="newPassword2" type="password" placeholder="Gjennta passord...">
-            <br>
-            <input type="submit" value="Endre">
-        </form>
-    </div>
-    <br>
-    <br>
-    <a href="/admin">Tilbake til forside</a>
-    <a href="/admin">Gå til Salaby's lærersider</a>
+    <a id="returnToMainPage" href="/admin"><img src="/public/img/pil.png" width="15px">Tilbake til forside</a>
+    <a id ="learningPages" href="http://larer.salaby.no/" >Gå til Salaby's lærersider<img src="/public/img/pil.png" width="15px"></a>
 </div>
