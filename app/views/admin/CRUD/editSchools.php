5256a869bc47d2ae6ea9862be7b590d7070b739e
Simen Fonnes <fonsim13@student.westerdals.no>
2015-05-04 19:10:53 +0200

    Ryddet + refactor i admin generelt.

--- app/views/admin/CRUD/editSchools.php
+++ app/views/admin/CRUD/editSchools.php
@@ -9,41 +9,47 @@
 <div id="content" class="widthConstrained">
     <?php include $this->root."/app/views/admin/PartialViews/adminMenu.php"?>
     <div class="tableBG">
-        <h2><?php echo $this->school['name']; ?></h2>
+        <h2 id="mainHeadline"><?php echo $this->school['name']; ?></h2>
 
-        <form method="post" action="/admin/updateSchool">
-            <input name="id" type="hidden" value="<?php echo $this->school['id']; ?>">
-            <input name="name" type="text" placeholder="Skolenavn" value="<?php echo $this->school['name']; ?>">
-            <input name="fylke" type="text" placeholder="Fylke" value="<?php echo $this->school['fylke']; ?>">
-            <input name="kommune" type="text" placeholder="Kommune" value="<?php echo $this->school['kommune']; ?>">
-            <input type="submit" value="Oppdater">
-        </form>
+        <div id="updateSchoolDiv">
+            <h4>Legg til ny bruker</h4>
+            <form method="post" action="/admin/updateSchool">
+                <input name="id" type="hidden" value="<?php echo $this->school['id']; ?>">
+                <input required class="smoothInputField" name="name" type="text" placeholder="Skolenavn" value="<?php echo $this->school['name']; ?>">
+                <input required class="smoothInputField" name="fylke" type="text" placeholder="Fylke" value="<?php echo $this->school['fylke']; ?>">
+                <input required class="smoothInputField" name="kommune" type="text" placeholder="Kommune" value="<?php echo $this->school['kommune']; ?>">
+                <br>
+                <input type="submit" value="Endre">
+            </form>
+        </div>
 
-        <div class="relationTable">
+
+        <div class="relationTable" id="newUserDiv">
+            <h4>Legg til ny bruker</h4>
+            <form method="post" action="/admin/addSchoolUser">
+                <input type="hidden" name="schoolid" value="<?php echo $this->school['id'];?>">
+                <input class="smoothInputField" type="text" name="username" placeholder="Brukernavn" required><br>
+                <input class="smoothInputField" type="password" name="password" placeholder="Passord" required><br>
+                <input class="smoothInputField" type="email" name="email" placeholder="E-post" required><br>
+                <input type="submit" value="Legg til">
+            </form>
+        </div>
+
+        <div class="relationTable" id="listOfSchoolUsersDiv">
             <h4>Skolebrukere</h4>
             <table>
                 <thead>
                     <th>Brukernavn</th>
-                    <th>Slett</th>
+                    <th></th>
                 </thead>
                 <?php foreach($this->schoolUsers as $user) { ?>
                     <tr>
                         <td><?php echo $user['username']?></td>
-                        <td onclick="deleteUser(this, '<?php echo $user['username']?>')">X</td>
+                        <td class="deleteBtn" title="Slett skolebruker" onclick="deleteUser(this, '<?php echo $user['username']?>')"></td>
                     </tr>
                 <?php } ?>
 
             </table>
-        </div>
-        <div class="relationTable">
-            <h4>Legg til skolebruker</h4>
-            <form method="post" action="/admin/addSchoolUser">
-                <input type="hidden" name="schoolid" value="<?php echo $this->school['id'];?>">
-                <input type="text" name="username" placeholder="Brukernavn"><br>
-                <input type="password" name="password" placeholder="Passord"><br>
-                <input type="email" name="email" placeholder="Epost"><br>
-                <input type="submit" value="Legg til skolebruker">
-            </form>
         </div>
     </div>
 </div>
