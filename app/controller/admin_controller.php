04ac71a7866fb7bb6115e228bbfad239baae031e
Simen Fonnes <fonsim13@student.westerdals.no>
2015-05-04 11:00:08 +0200

    Opplastning av fag, DONE

--- app/controller/admin_controller.php
+++ app/controller/admin_controller.php
@@ -95,14 +95,14 @@
         }
 
         public function actuallyAddSubject () {
-            echo $_FILES["pic"]["name"];
-            if (picUpload($this->root)) {
+            $fileName = picUpload($this->root);
+            if ($fileName) {
+                addNewSubject($_POST['fagnavn'], $_POST['klasseTrinn'], $fileName);
                 echo 'Filen ble lastet opp!';
             } else {
                 echo 'Filen ble IKKE lastet opp.';
             }
             /*
-            addSubject($_POST['fagnavn'], $_POST['klasseTrinn'], $_POST['fileToUpload']);
             header("Location: /admin/administrateSubjects");
             exit;
             */
