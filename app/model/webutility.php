47cb387f71975fb04ca45cb67557f95f843de923
Fredrik <lobfre13@nith.no>
2015-05-07 17:10:36 +0200

    url slugify ÆØÅ fix

--- app/model/webutility.php
+++ app/model/webutility.php
@@ -1,13 +1,14 @@
 <?php
 
     function slugify($text){
-        $text = strtolower($text);
         $text = str_replace('-', '_', $text);
         $text = str_replace(' ', '-', $text);
         $text = str_replace('æ', 'ae', $text);
         $text = str_replace('ø', 'oe', $text);
         $text = str_replace('å', 'aa', $text);
-
+        $text = str_replace('Æ', 'AE', $text);
+        $text = str_replace('Ø', 'OE', $text);
+        $text = str_replace('Å', 'AA', $text);
 
         return $text;
     }
@@ -18,6 +19,9 @@
         $text = str_replace('ae', 'æ', $text);
         $text = str_replace('oe', 'ø', $text);
         $text = str_replace('aa', 'å', $text);
+        $text = str_replace('AE', 'Æ', $text);
+        $text = str_replace('OE', 'Ø', $text);
+        $text = str_replace('AA', 'Å', $text);
 
         return $text;
     }
